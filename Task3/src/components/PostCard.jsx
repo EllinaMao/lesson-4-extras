4ce0cb8c113c04aabed0f3898bb688b85e50e299
Olga <xotaoshi@gmail.com>
2025-12-20 01:43:34 +0200

    fsd

--- Task3/src/components/PostCard.jsx
+++ Task3/src/components/PostCard.jsx
@@ -1,4 +1,4 @@
-import style from "../assets/css/modules/PostCard.module.css"; 
+import style from "../assets/css/modules/PostCard.module.css";
 import Image from "./Image";
 import LikeButton from "./LikeButton";
 
@@ -6,15 +6,11 @@ function PostCard({ title, date, text, image }) {
   return (
     <article className={style["post-card"]}>
       <div className={style["post-content"]}>
-        
-
-            
         <h3 className={style["post-title"]}>{title}</h3>
         <span className={style["post-date"]}>{date}</span>
         <p className={style["post-text"]}>{text}</p>
-        
         <Image src={image} alt={title} styleName="postImage" />
-        
+
         <LikeButton />
       </div>
     </article>
